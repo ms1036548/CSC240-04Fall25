dce1d57874fd10f1ed8fb684bd5ebff5b4b175a0
Maxim Khlobystov <MK1051988@wcupa.edu>
2025-10-30 19:16:40 -0400

    Added my Phase 2 work (local updates)

---
+++ ui-api/src/main/java/team/phase2/classapi/ClassApiApplication.java
@@ -0,0 +1,11 @@
+package team.phase2.classapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClassApiApplication.class, args);
+    }
+}
